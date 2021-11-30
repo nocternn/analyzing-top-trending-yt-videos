4ec13a3928321a0dd85e76505be48d193b8b92d5
Noctern <alexsky.gt12@gmail.com>
2021-12-01 01:11:07 +0700

    Added demo for questions 1+2

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
+from inspect import stack
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-import urllib.parse as p
-import re
 import os
 import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+CATEGORIES = {1: 'Film & Animation',\
+	2: 'Autos & Vehicles',\
+	10: 'Music',\
+	15: 'Pets & Animals',\
+	17: 'Sports',\
+	18: 'Short Movies',\
+	19: 'Travel & Events',\
+	20: 'Gaming',\
+	21: 'Videoblogging',\
+	22: 'People & Blogs',\
+	23: 'Comedy',\
+	24: 'Entertainment',\
+	25: 'News & Politics',\
+	26: 'Howto & Style',\
+	27: 'Education',\
+	28: 'Science & Technology',\
+	29: 'Nonprofits & Activism',\
+	30: 'Movies',\
+	31: 'Anime/Animation',\
+	32: 'Action/Adventure',\
+	33: 'Classics',\
+	34: 'Comedy',\
+	35: 'Documentary',\
+	36: 'Drama',\
+	37: 'Family',\
+	38: 'Foreign',\
+	39: 'Horror',\
+	40: 'Sci-Fi/Fantasy',\
+	41: 'Thriller',\
+	42: 'Shorts',\
+	43: 'Shows',\
+	44: 'Trailers'}
 
 
 def youtube_authenticate():
-	os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-	api_service_name = "youtube"
-	api_version = "v3"
-	client_secrets_file = "credentials.json"
+	os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+	api_service_name = 'youtube'
+	api_version = 'v3'
+	client_secrets_file = 'credentials.json'
 	creds = None
 	# the file token.pickle stores the user's access and refresh tokens, and is
 	# created automatically when the authorization flow completes for the first time
-	if os.path.exists("token.pickle"):
-		with open("token.pickle", "rb") as token:
+	if os.path.exists('token.pickle'):
+		with open('token.pickle', 'rb') as token:
 			creds = pickle.load(token)
 	# if there are no (valid) credentials availablle, let the user log in.
 	if not creds or not creds.valid:
@@ -29,60 +64,95 @@ def youtube_authenticate():
 			flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
 			creds = flow.run_local_server(port=0)
 			# save the credentials for the next run
-			with open("token.pickle", "wb") as token:
+			with open('token.pickle', 'wb') as token:
 				pickle.dump(creds, token)
 
 	return build(api_service_name, api_version, credentials=creds)
 
 def get_video_details(youtube, **kwargs):
 	return youtube.videos().list(
-		part="snippet,contentDetails,statistics",
-		chart="mostPopular",
+		part='snippet,statistics',
+		chart='mostPopular',
 		**kwargs
 	).execute()
 
-def print_video_infos(video_response):
-	for items in video_response.get("items"):
-		# get the snippet, statistics & content details from the video response
-		snippet         = items["snippet"]
-		statistics      = items["statistics"]
-		content_details = items["contentDetails"]
-		# get infos from the snippet
-		channel_title = snippet["channelTitle"]
-		title         = snippet["title"]
-		description   = snippet["description"]
-		publish_time  = snippet["publishedAt"]
-		# get stats infos
-		comment_count = statistics["commentCount"]
-		like_count    = statistics["likeCount"]
-		dislike_count = statistics["dislikeCount"]
-		view_count    = statistics["viewCount"]
-		# get duration from content details
-		duration = content_details["duration"]
-		print(f"""\
-			Title: {title}
-			Description: {description}
-			Channel Title: {channel_title}
-			Publish time: {publish_time}
-			Duration: {duration}
-			Number of comments: {comment_count}
-			Number of likes: {like_count}
-			Number of dislikes: {dislike_count}
-			Number of views: {view_count}
-			""")
-		print("----------------------------------------------------------------------");
+def plot_question_1(videos):
+	# Creating autocpt arguments
+	def func(pct, allvalues):
+		absolute = int(pct / 100.*np.sum(allvalues))
+		return "{:.1f}%\n({:d} g)".format(pct, absolute)
+	# create empty arrays to hold relevant data
+	categories = []
+	view_counts = []
+	# populate arrays
+	for video in videos:
+		categories.append(CATEGORIES[int(video['snippet']['categoryId'])])
+		try:
+			view_counts.append(int(video['statistics']['viewCount']))
+		except:
+			view_counts.append(0)
+	# create dataset and sort by total view count
+	dataset = pd.DataFrame(data={'categories': categories, 'view_count': view_counts})
+	dataset = dataset.groupby('categories').sum().sort_values(by=['view_count'], ascending=False)
+	# take top 5 categories
+	dataset_top = dataset[:5].copy()
+	# one entry for all other categories
+	dataset_others = pd.DataFrame(data={'view_count': [dataset['view_count'][5:].sum()]}, index=['Others'])
+	# combining top 5 with others
+	dataset = pd.concat([dataset_top, dataset_others])
+	# plot
+	dataset.plot(kind='pie', subplots=True,\
+		legend=False,\
+		shadow=True,\
+		autopct=lambda pct: func(pct, dataset['view_count'])\
+	)
+	plt.show()
+
+def plot_question_2(videos):
+	# create empty arrays to hold relevant data
+	categories = []
+	likes_count = []
+	dislikes_count = []
+	comments_count = []
+	# populate arrays
+	for video in videos:
+		categories.append(CATEGORIES[int(video['snippet']['categoryId'])])
+		try:
+			likes_count.append(int(video['statistics']['likeCount']))
+		except:
+			likes_count.append(0)
+		try:
+			dislikes_count.append(int(video['statistics']['dislikeCount']))
+		except:
+			dislikes_count.append(0)
+		try:
+			comments_count.append(int(video['statistics']['commentCount']))
+		except:
+			comments_count.append(0)
+	# create dataset and sort by total view count
+	dataset = pd.DataFrame(data={'categories': categories,\
+		'Likes': likes_count,\
+		'Dislikes': dislikes_count,\
+		'Comments': comments_count\
+	})
+	dataset = dataset.groupby('categories').sum().sort_values(\
+		by=['Likes', 'Dislikes', 'Comments'], ascending=False)
+	# plot
+	dataset.plot(kind="barh", stacked=True)
+	plt.show()
+
 
 if __name__ == '__main__':
 	# authenticate to YouTube API
 	youtube = youtube_authenticate()
-	# loop through result pages
-	next_token = None
-	while True:
-		# make API call to get video info
-		response = get_video_details(youtube, pageToken=next_token)
-		# print extracted video infos
-		print_video_infos(response)
-		# get next result page
-		next_token = response.get("nextPageToken")
-		if not next_token:
-			break
+	# make API call to get video info
+	response = get_video_details(youtube)
+	total_items = response.get('pageInfo')['totalResults']
+	items = response.get('items')
+	while len(items) < total_items:
+		response = get_video_details(youtube, pageToken=response.get('nextPageToken'))
+		items.extend(response.get('items'))
+	# first plot
+	plot_question_1(items)
+	# second plot
+	plot_question_2(items)
