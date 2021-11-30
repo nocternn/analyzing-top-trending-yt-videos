b174231462673689246766b2306b15f4a54d97c0
Noctern <alexsky.gt12@gmail.com>
2021-11-30 20:36:35 +0700

    Pull a list of most popular YT videos

--- main.py
+++ main.py
@@ -34,70 +34,55 @@ def youtube_authenticate():
 
 	return build(api_service_name, api_version, credentials=creds)
 
-def get_video_id_by_url(url):
-	"""
-	Return the Video ID from the video `url`
-	"""
-	# split URL parts
-	parsed_url = p.urlparse(url)
-	# get the video ID by parsing the query of the URL
-	video_id = p.parse_qs(parsed_url.query).get("v")
-	if video_id:
-		return video_id[0]
-	else:
-		raise Exception(f"Wasn't able to parse video URL: {url}")
-
 def get_video_details(youtube, **kwargs):
 	return youtube.videos().list(
 		part="snippet,contentDetails,statistics",
+		chart="mostPopular",
 		**kwargs
 	).execute()
 
 def print_video_infos(video_response):
-	items = video_response.get("items")[0]
-	# get the snippet, statistics & content details from the video response
-	snippet         = items["snippet"]
-	statistics      = items["statistics"]
-	content_details = items["contentDetails"]
-	# get infos from the snippet
-	channel_title = snippet["channelTitle"]
-	title         = snippet["title"]
-	description   = snippet["description"]
-	publish_time  = snippet["publishedAt"]
-	# get stats infos
-	comment_count = statistics["commentCount"]
-	like_count    = statistics["likeCount"]
-	dislike_count = statistics["dislikeCount"]
-	view_count    = statistics["viewCount"]
-	# get duration from content details
-	duration = content_details["duration"]
-	# duration in the form of something like 'PT5H50M15S'
-	# parsing it to be something like '5:50:15'
-	parsed_duration = re.search(f"PT(\d+H)?(\d+M)?(\d+S)", duration).groups()
-	duration_str = ""
-	for d in parsed_duration:
-		if d:
-			duration_str += f"{d[:-1]}:"
-	duration_str = duration_str.strip(":")
-	print(f"""\
-		Title: {title}
-		Description: {description}
-		Channel Title: {channel_title}
-		Publish time: {publish_time}
-		Duration: {duration_str}
-		Number of comments: {comment_count}
-		Number of likes: {like_count}
-		Number of dislikes: {dislike_count}
-		Number of views: {view_count}
-		""")
+	for items in video_response.get("items"):
+		# get the snippet, statistics & content details from the video response
+		snippet         = items["snippet"]
+		statistics      = items["statistics"]
+		content_details = items["contentDetails"]
+		# get infos from the snippet
+		channel_title = snippet["channelTitle"]
+		title         = snippet["title"]
+		description   = snippet["description"]
+		publish_time  = snippet["publishedAt"]
+		# get stats infos
+		comment_count = statistics["commentCount"]
+		like_count    = statistics["likeCount"]
+		dislike_count = statistics["dislikeCount"]
+		view_count    = statistics["viewCount"]
+		# get duration from content details
+		duration = content_details["duration"]
+		print(f"""\
+			Title: {title}
+			Description: {description}
+			Channel Title: {channel_title}
+			Publish time: {publish_time}
+			Duration: {duration}
+			Number of comments: {comment_count}
+			Number of likes: {like_count}
+			Number of dislikes: {dislike_count}
+			Number of views: {view_count}
+			""")
+		print("----------------------------------------------------------------------");
 
 if __name__ == '__main__':
 	# authenticate to YouTube API
 	youtube = youtube_authenticate()
-	video_url = "https://www.youtube.com/watch?v=jNQXAC9IVRw&ab_channel=jawed"
-	# parse video ID from URL
-	video_id = get_video_id_by_url(video_url)
-	# make API call to get video info
-	response = get_video_details(youtube, id=video_id)
-	# print extracted video infos
-	print_video_infos(response)
+	# loop through result pages
+	next_token = None
+	while True:
+		# make API call to get video info
+		response = get_video_details(youtube, pageToken=next_token)
+		# print extracted video infos
+		print_video_infos(response)
+		# get next result page
+		next_token = response.get("nextPageToken")
+		if not next_token:
+			break
